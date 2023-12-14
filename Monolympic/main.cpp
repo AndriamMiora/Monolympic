d0fbb84a7c6c8a99d11d75fbfe2cee2bf313abe2
Ashley <koloina.andrianarivo@icloud.com>
2023-12-14 22:22:38 +0100

    update interface graphique

--- Monolympic/main.cpp
+++ Monolympic/main.cpp
@@ -7,31 +7,27 @@
 using namespace std;
 
 int main(){
-
-
-
-// Pour créer une fenêtre de jeu
+    // Pour créer une fenêtre de jeu
     sf::RenderWindow window(sf::VideoMode(1280, 720),
     "by Andrianarivo Ashley & Andriambolo-nivo Miora",
     sf::Style::Close | sf::Style::Titlebar); // Style de la fenêtre : Close = bouton fermer, Titlebar = barre de titre
 
-// Pour charger une police de caractère
+    // Pour charger une police de caractère
     sf::Font stdFont;
     stdFont.loadFromFile("police1.otf");
 
-// Pour charger une image
+    // Pour charger une image d'icône de jeu 
     sf::Image favicon;
     favicon.loadFromFile("assets/favicon.png");
     window.setIcon(314, 229, favicon.getPixelsPtr()); // 314x229 est la taille de l'image favicon.png
 
-// Pour charger une image de fond
+    // Pour charger une image de fond
     sf::RectangleShape introScreen(sf::Vector2f(1280.0f, 720.0f));
     sf::Texture introScreenTexture;
     introScreenTexture.loadFromFile("font.jpeg");
     introScreen.setTexture(&introScreenTexture);
 
-
-// Pour afficher du texte
+    // Pour afficher du texte
     sf::Text introScreenText;
     introScreenText.setFont(stdFont);
     introScreenText.setCharacterSize(50);
@@ -40,7 +36,7 @@ int main(){
     introScreenText.setFillColor(sf::Color::White);
     introScreenText.setString("MONOLYMPIC GAMES");
 
-// Pour afficher du texte
+
     sf::Text introScreenText2;
     introScreenText2.setFont(stdFont);
     introScreenText2.setCharacterSize(20);
@@ -48,16 +44,16 @@ int main(){
     introScreenText2.setFillColor(sf::Color::Black);
     introScreenText2.setString("by Andrianarivo Ashley & Andriambolo-nivo Miora\n");
 
-// créer un bouton pour commencer le jeu
+    // créer un bouton pour commencer le jeu
     sf::Texture newGameButtonTexture;
-    newGameButtonTexture.loadFromFile("assets/new-game-button.png");
+    newGameButtonTexture.loadFromFile("assets/Start.png");
     sf::RectangleShape newGameButton(sf::Vector2f(316.0f, 94.0f));
     newGameButton.setTexture(&newGameButtonTexture);
     newGameButton.setPosition(sf::Vector2f(480.0f, 210.0f));
 
     bool gameStarted = false;
 
-   while (!gameStarted) {
+    while (!gameStarted) {
         sf::Event evt;
         while (window.pollEvent(evt)) {
             if (evt.type == sf::Event::Closed) {
@@ -79,9 +75,7 @@ int main(){
         window.display();
     }
 
-
-
-    // Pour charger une image de fond
+    // Pour charger une image de fond pour la sélection des noms des joueurs et des pions
     sf::Texture selectionScreenTexture;
     selectionScreenTexture.loadFromFile("assets/selection-screen.png");
     sf::RectangleShape selectionScreen(sf::Vector2f(1280.0f, 720.0f));
@@ -89,101 +83,174 @@ int main(){
 
     // créer un bouton pour entrer le nom du joueur
     sf::Texture enternametexture;
-    enternametexture.loadFromFile("Entername.png");
+    enternametexture.loadFromFile("assets/entrer_nom.png");
     sf::RectangleShape enternamebutton(sf::Vector2f(316.0f, 94.0f));
     enternamebutton.setTexture(&enternametexture);
     enternamebutton.setPosition(sf::Vector2f(480.0f, 210.0f));
  
-    // Name input box
+    // box pour entrer le nom du joueur
     sf::RectangleShape nameInputBox(sf::Vector2f(300.0f, 50.0f));
     nameInputBox.setFillColor(sf::Color::White);
-    nameInputBox.setPosition(sf::Vector2f(500.0f, 400.0f));
+    nameInputBox.setPosition(sf::Vector2f(480.0f, 300.0f));
 
-    // Name input text
+    // texte pour entrer le nom du joueur
     sf::Text nameInputText;
     nameInputText.setFont(stdFont);
     nameInputText.setCharacterSize(20);
-    nameInputText.setPosition(sf::Vector2f(510.0f, 410.0f));
+    nameInputText.setPosition(sf::Vector2f(480.0f, 300.0f));
     nameInputText.setFillColor(sf::Color::Black); // Définir la couleur du texte en noir
 
     // Ajout bouton pour valider le nom du joueur
     sf::Texture validatetexture;
-    validatetexture.loadFromFile("Validate.png");
+    validatetexture.loadFromFile("assets/validate.png");
     sf::RectangleShape validatebutton(sf::Vector2f(316.0f, 94.0f));
     validatebutton.setTexture(&validatetexture);
-    validatebutton.setPosition(sf::Vector2f(480.0f, 210.0f));
+    validatebutton.setPosition(sf::Vector2f(480.0f, 350.0f));
     
 
-    // Keeps the choice that the user has made.
-    // 1 --> New Game
+    if (gameStarted) {
+        bool nameEntered = false;
+        string playerName = "";
+        while (!nameEntered) {
+            sf::Event evt;
+            while (window.pollEvent(evt)) {
+                if (evt.type == sf::Event::Closed) {
+                    window.close();
+                }
+                if (evt.type == sf::Event::TextEntered) {
+                    if (evt.text.unicode >= 32 && evt.text.unicode <= 126) {
+                        playerName += static_cast<char>(evt.text.unicode);
+                        nameInputText.setString(playerName);
+                    }
+                    else if (evt.text.unicode == 8 && playerName.length() > 0) {
+                        playerName.pop_back();
+                        nameInputText.setString(playerName);
+                    }
+                }
+                if (evt.type == sf::Event::MouseButtonPressed && evt.mouseButton.button == sf::Mouse::Left) {
+                    sf::Vector2f mousePos = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+                    if (validatebutton.getGlobalBounds().contains(mousePos)) {
+                        nameEntered = true;
+                    }
+                }
+            }
 
-    int choice = -1;
+            window.clear();
+            window.draw(selectionScreen);
+            window.draw(nameInputBox);
+            window.draw(nameInputText);
+            window.draw(enternamebutton);
+            window.draw(validatebutton);
+            window.display();
+        }
+    }
 
+    // Choix du pions : 4 choix de pions
 
-    while (window.isOpen() && gameStarted && choice == -1) {
+    sf::Texture pion1texture;
+    pion1texture.loadFromFile("assets/player1.png");
+    sf::RectangleShape pion1(sf::Vector2f(50.0f, 50.0f));
+    pion1.setTexture(&pion1texture);
+    pion1.setPosition(sf::Vector2f(480.0f, 400.0f));
+
+    sf::Texture pion2texture;
+    pion2texture.loadFromFile("assets/player2.png");
+    sf::RectangleShape pion2(sf::Vector2f(50.0f, 50.0f));
+    pion2.setTexture(&pion2texture);
+    pion2.setPosition(sf::Vector2f(480.0f, 450.0f));
+
+    sf::Texture pion3texture;
+    pion3texture.loadFromFile("assets/player3.png");
+    sf::RectangleShape pion3(sf::Vector2f(50.0f, 50.0f));
+    pion3.setTexture(&pion3texture);
+    pion3.setPosition(sf::Vector2f(480.0f, 500.0f));
+
+   
+
+string playerPion = "";
+// Code pour la première boucle (sélection du pion)
+if (gameStarted) {
+    bool pionSelected = false;
+    
+    while (!pionSelected && window.isOpen()) {
         sf::Event evt;
         while (window.pollEvent(evt)) {
             if (evt.type == sf::Event::Closed) {
                 window.close();
             }
-            if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+            if (evt.type == sf::Event::MouseButtonPressed && evt.mouseButton.button == sf::Mouse::Left) {
                 sf::Vector2f mousePos = window.mapPixelToCoords(sf::Mouse::getPosition(window));
-                sf::FloatRect enternamebuttonBounds = enternamebutton.getGlobalBounds();
-                if (enternamebuttonBounds.contains(mousePos)) {
-                    choice = 1;
-                    bool nameEntered = false;
-                    sf::String playerName;
-
-                    while (!nameEntered && window.isOpen()) {
-                        sf::Event nameEvent;
-                        while (window.pollEvent(nameEvent)) {
-                            if (nameEvent.type == nameEvent.Closed) {
-                                window.close();
-                            }
-                            else if (nameEvent.type == sf::Event::TextEntered) {
-                                if (nameEvent.text.unicode < 128) {
-                                    if (nameEvent.text.unicode == 8) { // Backspace
-                                        if (!playerName.isEmpty()) {
-                                            playerName.erase(playerName.getSize() - 1, 1);
-                                        }
-                                    }
-                                    else if (nameEvent.text.unicode == 13) { // Enter
-                                        if (!playerName.isEmpty()) {
-                                            nameEntered = true;
-                                        }
-                                    }
-                                    else {
-                                        playerName += nameEvent.text.unicode;
-                                    }
-                                }
-                            }
-                        }
-
-                        // Affiche le nom du joueur en cours de saisie
-                        nameInputText.setString(playerName);
-
-                        window.clear();
-                        window.draw(selectionScreen);
-                        window.draw(enternamebutton);
-                        window.draw(nameInputBox);
-                        window.draw(nameInputText);
-                        window.display();
-                    }
-
+                if (pion1.getGlobalBounds().contains(mousePos)) {
+                    pionSelected = true;
+                    playerPion = "pion1";
                 }
-
+                if (pion2.getGlobalBounds().contains(mousePos)) {
+                    pionSelected = true;
+                    playerPion = "pion2";
+                }
+                if (pion3.getGlobalBounds().contains(mousePos)) {
+                    pionSelected = true;
+                    playerPion = "pion3";
+                }
+                
             }
-
         }
+
         window.clear();
         window.draw(selectionScreen);
-        window.draw(enternamebutton);
+        window.draw(pion1);
+        window.draw(pion2);
+        window.draw(pion3);
+
         window.display();
     }
-
-    window.close();
-    return 0;
 }
 
+// Code pour la deuxième boucle (affichage du plateau de jeu)
+if (gameStarted) {
+    sf::Texture boardtexture;
+    boardtexture.loadFromFile("Board.jpeg");
+    sf::RectangleShape board(sf::Vector2f(1280.0f, 720.0f));
+    board.setTexture(&boardtexture);
 
+    bool boardSelected = false;
+    string playerBoard = "";
+    while (!boardSelected && window.isOpen()) {
+        sf::Event evt;
+        while (window.pollEvent(evt)) {
+            if (evt.type == sf::Event::Closed) {
+                window.close();
+            }
+            if (evt.type == sf::Event::MouseButtonPressed && evt.mouseButton.button == sf::Mouse::Left) {
+                sf::Vector2f mousePos = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+                if (board.getGlobalBounds().contains(mousePos)) {
+                    boardSelected = true;
+                    playerBoard = "board";
+                }
+            }
+        }
 
+        window.clear();
+        window.draw(board);
+        // afficher le pion du joueur en bas à droite du plateau
+        if (playerPion == "pion1") {
+            pion1.setPosition(sf::Vector2f(1000.0f, 400.0f));
+            window.draw(pion1);
+        }
+        if (playerPion == "pion2") {
+            pion2.setPosition(sf::Vector2f(1000.0f, 400.0f));
+            window.draw(pion2);
+        }
+        if (playerPion == "pion3") {
+            pion3.setPosition(sf::Vector2f(1000.0f, 400.0f));
+            window.draw(pion3);
+        }
+    
+        window.display();
+    }
+}
+
+window.clear();
+window.close();
+return 0;
+}
